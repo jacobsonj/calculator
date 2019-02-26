ca3afa400afefa953fac98ce1a6853a19d4006f9
Justin Jacobson <justinjacobson0118@gmail.com>
2019-02-25 19:20:44 -0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
-function add(a, b) {
-  return a + b;
+var numberArray = document.getElementsByClassName('num-bg');
+var operatorArray = document.getElementsByClassName('btn-style opera-bg operator');
+var result = document.getElementById('result');
+var equal = document.getElementById('eqn-bg');
+var clear = document.getElementById('delete');
+
+addNumberEventListener(numberArray);
+addOperatorEventListener(operatorArray);
+
+equal.addEventListener('click', function(){
+  var equation = result.innerHTML;
+  equation = equation.toString();
+  var solution = eval(equation);
+  result.innerHTML = solution;
+  
+  console.log(result.innerHTML);
+})
+
+function addNumberEventListener(arr){
+  for(var i = 0; i < arr.length; i++){
+    arr[i].addEventListener('click', function(event){
+      result.innerHTML += event.target.value;
+      
+      console.log(event.target.value);
+    })
+  }
 }
+
+function addOperatorEventListener(arr){
+  for(var i = 0; i < arr.length; i++){
+    arr[i].addEventListener('click', function(event){
+      result.innerHTML += event.target.value;
+      
+      console.log(event.target.value);
+    })
+  }
+}
+
+clear.addEventListener('click', function(){
+  result.innerHTML = '';
+})
