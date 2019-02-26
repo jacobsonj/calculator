58b809e9a38dd45e6c5217df4c36a1d501536504
Justin Jacobson <justinjacobson0118@gmail.com>
2019-02-25 21:56:02 -0700

    made it so keyboard works. started working on flag function

--- src/index.js
+++ src/index.js
@@ -3,35 +3,81 @@ var operatorArray = document.getElementsByClassName('btn-style opera-bg operator
 var result = document.getElementById('result');
 var equal = document.getElementById('eqn-bg');
 var clear = document.getElementById('delete');
+var operators = ['+', '-', '*', '/', '.'];
+
 
 addNumberEventListener(numberArray);
 addOperatorEventListener(operatorArray);
 
-equal.addEventListener('click', function(){
+function flagged(){
+  var testResult = result.innerHTML.split('');
+  if(operators.includes(testResult[testResult.length-1])){
+    return true;
+  }
+}
+
+// to make equals sign work
+function onEnterPress(){
   var equation = result.innerHTML;
   equation = equation.toString();
   var solution = eval(equation);
   result.innerHTML = solution;
   
-  console.log(result.innerHTML);
+  // console.log(result.innerHTML);
+}
+equal.addEventListener('click', onEnterPress);
+
+// to make keyboard work
+window.addEventListener('keyup', function(event){
+  var num = parseInt(event.key);
+  var oper = event.key;
+  if(event.keyCode === 13){
+    if(flagged() === true){
+      alert('dont work');
+    }
+    onEnterPress(); 
+  }
+  else if(!Number.isNaN(num)){
+    
+    onNumberPress(num);
+  }
+  else if(operators.includes(oper)){
+    if(flagged() === true){
+      alert('dont work');
+    }
+    onOperatorPress(oper);
+  }
+  // console.log(event);
 })
+
+window.addEventListener('keydown', function(event){
+  event.preventDefault();
+});
+
+function onNumberPress(value){
+  result.innerHTML += value;
+  
+  // console.log(value);
+}
 
 function addNumberEventListener(arr){
   for(var i = 0; i < arr.length; i++){
     arr[i].addEventListener('click', function(event){
-      result.innerHTML += event.target.value;
-      
-      console.log(event.target.value);
+      onNumberPress(event.target.value);
     })
   }
+}
+
+function onOperatorPress(value){
+  result.innerHTML += value;
 }
 
 function addOperatorEventListener(arr){
   for(var i = 0; i < arr.length; i++){
     arr[i].addEventListener('click', function(event){
-      result.innerHTML += event.target.value;
+      onOperatorPress(event.target.value);
       
-      console.log(event.target.value);
+      // console.log(event.target.value);
     })
   }
 }
