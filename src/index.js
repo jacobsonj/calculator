c6d0001a754dfadfabc39c2c6eddd8adace9b40b
Justin Jacobson <justinjacobson0118@gmail.com>
2019-02-26 21:02:59 -0700

    reformatted to be more organized. added condition for if result already has . after equal is pressed, canDecimal = false

--- src/index.js
+++ src/index.js
@@ -4,14 +4,57 @@ var result = document.getElementById('result');
 var equal = document.getElementById('eqn-bg');
 var clear = document.getElementById('delete');
 var operators = ['+', '-', '*', '/', '.'];
-
-
-addNumberEventListener(numberArray);
-addOperatorEventListener(operatorArray);
-
 var canDecimal = true;
 var canOperator = false;
 
+// buttons
+// numbers
+addNumberEventListener(numberArray);
+function addNumberEventListener(arr){
+  for(var i = 0; i < arr.length; i++){
+    arr[i].addEventListener('click', function(event){
+      addToResult(event.target.value);
+      canOperator = true;
+    })
+  }
+}
+
+// operators
+addOperatorEventListener(operatorArray);
+function addOperatorEventListener(arr){
+  for(var i = 0; i < arr.length; i++){
+    arr[i].addEventListener('click', function(event){
+      checkNaN(event.target.value);
+      
+      // console.log(event.target.value);
+    })
+  }
+}
+
+// equals
+equal.addEventListener('click', onEnterPress);
+
+// add event listener to keys
+window.addEventListener('keyup', function(event){
+  var num = parseInt(event.key);
+  var oper = event.key;
+  // equals
+  if(event.keyCode === 13){
+    onEnterPress(); 
+    // canDecimal = true;
+  }
+  // numbers
+  else if(!Number.isNaN(num)){
+    addToResult(num);
+    canOperator = true;
+  }
+  // operators
+  else if(operators.includes(oper)){
+    checkNaN(oper);
+  }
+});
+
+// in case of operators being used twice in a row, replaces old operator with new
 function replaceLast(value){
   var resultArray = result.innerHTML.split('');
   resultArray[resultArray.length-1] = value;
@@ -19,36 +62,20 @@ function replaceLast(value){
   result.innerHTML = replacedStr; 
 }
 
-// to make equals sign work
+// evaluate string input in result window
 function onEnterPress(){
   var equation = result.innerHTML;
   equation = equation.toString();
   var solution = eval(equation);
   result.innerHTML = solution;
-  canDecimal = true;
+  if(result.innerHTML.includes('.')){
+    canDecimal = false;
+  }
+  else{canDecimal = true;}
   // console.log(result.innerHTML);
 }
 
-equal.addEventListener('click', onEnterPress);
-
-// to make keyboard work
-window.addEventListener('keyup', function(event){
-  var num = parseInt(event.key);
-  var oper = event.key;
-  if(event.keyCode === 13){
-    onEnterPress(); 
-    // canDecimal = true;
-  }
-  else if(!Number.isNaN(num)){
-    addToResult(num);
-    canOperator = true;
-  }
-  else if(operators.includes(oper)){
-    checkNaN(oper);
-  }
-  // console.log(event);
-})
-
+// to prevent operators and decimal from being input invalidly
 function checkNaN(oper){
   console.log(oper);
   if(oper === '.' && canDecimal === true){
@@ -71,35 +98,16 @@ function checkNaN(oper){
     }
 }
 
-window.addEventListener('keydown', function(event){
-  event.preventDefault();
-});
-
+// display input in result window
 function addToResult(value){
   result.innerHTML += value;
-  
-  // console.log(value);
 }
 
-function addNumberEventListener(arr){
-  for(var i = 0; i < arr.length; i++){
-    arr[i].addEventListener('click', function(event){
-      addToResult(event.target.value);
-      canOperator = true;
-    })
-  }
-}
-
-function addOperatorEventListener(arr){
-  for(var i = 0; i < arr.length; i++){
-    arr[i].addEventListener('click', function(event){
-      checkNaN(event.target.value);
-      
-      // console.log(event.target.value);
-    })
-  }
-}
-
+// clear button
 clear.addEventListener('click', function(){
   result.innerHTML = '';
 })
+
+window.addEventListener('keydown', function(event){
+  event.preventDefault();
+});
