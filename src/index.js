038be403cc0f78c360dd22ae00941afa7a3fec15
Justin Jacobson <justinjacobson0118@gmail.com>
2019-03-18 18:38:24 -0600

    worked on equation array

--- src/index.js
+++ src/index.js
@@ -75,19 +75,19 @@ function replaceLast(value) {
 
 // evaluate string input in result window
 function handleEqual() {
-  var equation = result.innerHTML;
-  equation = equation.toString();
+  newValues.push(currNumber);
+  equation = newValues.join('');
+  // equation = newValues.toString();
   var solution = eval(equation);
   result.innerHTML = solution;
-  newValues = [solution];
-  currNumber = '';
+  newValues = [];
+  currNumber = solution;
   if (result.innerHTML.includes('.')) {
     canDecimal = false;
   }
   else { canDecimal = true; }
   // console.log(result.innerHTML);
 }
-
 
 function handleOperator(oper) {
   console.log(oper);
@@ -112,7 +112,6 @@ function handleOperator(oper) {
       canDecimal = true;
     }
   }
-
 }
 
 // display input in result window
@@ -134,12 +133,23 @@ window.addEventListener('keydown', function (event) {
 });
 
 posNeg.addEventListener('click', function () {
-
+  currNumber = eval(-currNumber);
+  // newValues.push(currNumber);
+  // posNegChanger();
 });
 
+// function posNegChanger(){
+//   for(var i = newValues.length; i >= 0; i--){
+//     if(!Number.isNaN(newValues[i]) || newValues[i] === '.'){
+//       return;
+//     }
+//     else if(operations.includes(newValues[i])){
+
+//     }
+//   }
+// }
 
 function storeValues(digit) {
-
   if (operations.includes(digit)) {
     currOperator = digit;
     if (currNumber === '') {
@@ -149,7 +159,6 @@ function storeValues(digit) {
       newValues.push(currNumber)
     }
     currNumber = '';
-
   }
   else if (!Number.isNaN(digit)) {
     digit = digit.toString();
@@ -171,181 +180,6 @@ function storeValues(digit) {
       newValues.push(currOperator)
     }
     currOperator = '';
-
   }
-console.log(newValues);
+  console.log(newValues);
 }
-
-
-
-
-
-
-
-
-
-
-// function init(){
-//   var numbers = document.getElementsByClassName('num');
-//   var operatorButtons = document.getElementsByClassName('btn-style operator');
-//   var result = document.getElementById('result');
-//   var equal = document.getElementById('eqn-bg');
-//   var clear = document.getElementById('delete');
-//   var posNeg = document.getElementById('pos-neg');
-//   var main = document.getElementById('main');
-
-//   addNumberEventListener(numbers, result);
-//   addOperatorEventListener(operatorArray, result);
-//   // addEqualEventListener
-// }
-// if(document.getElementById('main')){
-//   init();
-// }
-
-// var operators = ['+', '-', '*', '/', '.'];
-// var operations = ['+', '-', '*', '/']
-// var canDecimal = true;
-// var canOperator = false;
-// var newValues = [];
-
-
-// // main.addEventListener('click', function(event) {
-// //   console.log(event)
-// // })
-// // buttons
-// // numbers
-
-// function addNumberEventListener(numbers, screen) {
-//   for (var i = 0; i < numbers.length; i++) {
-//     numbers[i].addEventListener('click', function (event) {
-//       handleNumber(event.target.value, screen);
-//     });
-//   }
-// }
-
-// function handleNumber(num, screen){
-//   addToResult(num, screen);
-//   canOperator = true;
-// }
-
-// // operators
-
-// function addOperatorEventListener(arr, screen) {
-//   for (var i = 0; i < arr.length; i++) {
-//     arr[i].addEventListener('click', function (event) {
-//       handleOperator(event.target.value, screen);
-
-//       // console.log(event.target.value);
-//     })
-//   }
-// }
-
-// equals
-// equal.addEventListener('click', handleEqual);
-
-// // add event listener to keys
-// document.addEventListener('keyup', function (event) {
-//   var num = parseInt(event.key);
-//   var oper = event.key;
-//   // equals
-//   if (event.keyCode === 13) {
-//     onEnterPress();
-//     // canDecimal = true;
-//   }
-//   // numbers
-//   else if (!Number.isNaN(num)) {
-//     handleNumber(num, screen);
-//     canOperator = true;
-//     console.log(num);
-//   }
-//   // operators
-//   else if (operators.includes(oper, screen)) {
-//     checkNaN(oper, screen);
-//   }
-// });
-
-// // in case of operators being used twice in a row, replaces old operator with new
-// function replaceLast(value, screen) {
-//   var resultArray = screen.innerHTML.split('');
-//   resultArray[resultArray.length - 1] = value;
-//   topBarArray[topBarArray.length - 1] = value;
-//   replacedStr = resultArray.join('');
-//   screen.innerHTML = replacedStr;
-// }
-
-// // evaluate string input in result window
-// function onEnterPress() {
-//   var equation = result.innerHTML;
-//   equation = equation.toString();
-//   var solution = eval(equation);
-//   result.innerHTML = solution;
-//   topBarArray =[solution];
-//   if (result.innerHTML.includes('.')) {
-//     canDecimal = false;
-//   }
-//   else { canDecimal = true; }
-//   // console.log(result.innerHTML);
-// }
-
-// // to prevent operators and decimal from being input invalidly
-// function checkNaN(oper, screen) {
-//   console.log(oper);
-//   if (oper === '.' && canDecimal === true) {
-//     addToResult(oper, screen);
-//     canDecimal = false;
-//   }
-//   else if (oper === '.' && canDecimal === false) {
-//     return;
-//   }
-//   else if (oper === '+' || oper === '-' || oper === '*' || oper === '/') {
-//     if (canOperator) {
-//       console.log('addToResult');
-//       addToResult(oper, screen);
-//       canDecimal = true;
-//       canOperator = false;
-//     } else {
-//       console.log('replaceLast');
-//       replaceLast(oper, screen);
-//       canDecimal = true;
-//     }
-//   }
-// }
-
-// // // display input in result window
-// function addToResult(value, screen) {
-//   topBarArray.push(value);
-//   screen.innerHTML += value;
-// }
-
-// clear button
-// clear.addEventListener('click', function () {
-//   result.innerHTML = '';
-//   topBarArray = [];
-// })
-
-// window.addEventListener('keydown', function (event) {
-//   event.preventDefault();
-// });
-
-// posNeg.addEventListener('click', function(){
-//   var last = findLastNumber();
-//   last = eval(-last);
-//   topBarArray.splice(topBarArray.length - findLastNumber().length);
-//   last = last + '';
-//   for(var i = 0; i < last.length; i++){
-//     topBarArray.push(last[i]);
-//   }
-//   result.innerHTML = topBarArray.join('');
-// });
-
-// function findLastNumber(){
-//   var num = '';
-//   for(var i = topBarArray.length - 1; i >= 0; i--){
-//     var current = topBarArray[i];
-//     if(operations.includes(current)){
-//       break
-//     }
-//     else{num = (current + '') + num}
-//   }
-//   return num
-// }
